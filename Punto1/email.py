9993e2b62aa297dfbc9bffd2f1bd59673dae7f40
andy10134 <andy10134@hotmail.com>
2020-04-11 19:24:03 -0300

    Modulo email terminado

--- Punto1/email.py
+++ Punto1/email.py
@@ -1,33 +1,26 @@
 class Email():
-    __idCuenta =''
-    __dominio =''
-    __extension =''
+    __idCuenta    =''
+    __dominio     =''
+    __extension   =''
     __contrasenia =''
 
-#Constructor de la clase email
-    def __init__ (self, idCuenta,dominio, extension, contrasenia):
+    #Constructor de la clase email
+    def __init__ (self, idCuenta = '', dominio ='', extension = '', contrasenia = ''):
         self.__idCuenta    = idCuenta
         self.__dominio     = dominio
         self.__extension   = extension
         self.__contrasenia = contrasenia
-#Obtener Dominio del objeto
+    #Obtener Dominio del objeto
     def getDominio(self):
         return self.__dominio
-#Retornar Email
+    #Retornar Email
     def retornaEmail(self):
         return self.__idCuenta + '@' + self.__dominio + self.__extension
-#Crear Cuenta
+    #Crear Cuenta
     def crearCuenta(self, direccion):
-        direccion = direccion.split('@')
-        self.__idCuenta = direccion[0]
-        direccion = direccion[1].split('.')
-        self.__dominio = direccion[0]
-        self.__extension = direccion[1]
-
-
-        
-        return self.__extension
-
-if __name__ == "__main__":
-    email = Email('jorge','dsadsa','.com', 'dsads')
-    print(email.crearCuenta('jorge@gmail.com'))
+        direccion          = direccion.split('@')
+        self.__idCuenta    = direccion[0]
+        direccion          = direccion[1].split('.')
+        self.__dominio     = direccion[0]
+        self.__extension   = direccion[1]
+        self.__contrasenia = input('Ingrese password de {} :'.format(self.__idCuenta +'@'+ self.__dominio +'.'+self.__extension))
