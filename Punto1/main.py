014aa4d6f1e7a6e888967308d73993e5ac54c7d6
andy10134 <andy10134@hotmail.com>
2020-04-12 10:38:27 -0300

    Cambios en main

--- Punto1/main.py
+++ Punto1/main.py
@@ -1,12 +1,14 @@
+import csv
 import email as E
+
 if __name__ == "__main__":
     print('Ingrese los siguientes datos:')
-    nombre= input('nombre: ')
-    direccion= input('direccion email: ')
-    email = E.Email()
+    nombre     = input('nombre: ')
+    direccion  = input('direccion email: ')
+    email      = E.Email()
     email.crearCuenta(direccion)
     print('Estimado {} te enviaremos tus mensajes a la dirección {}'.format(nombre, direccion))
     email.cambiarContrasenia(input("Ingrese su clave para comenzar con el cambio de pass: "))
-    otroemail= E.Email()
-    otroemail.crearCuenta('wicc2019@unsj-cuim.edu')
-    print('FELICITACIONES FUNCIONA')
+    otroemail  = E.Email()
+    archivo    = open('direccionesgmai.csv')
+    reader     = csv.reader(archivo, delimiter = ';')
