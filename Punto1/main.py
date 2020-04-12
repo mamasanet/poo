b40d8c2b832fb0cc5cd3873d31d7abb1ead1cc8c
andy10134 <andy10134@hotmail.com>
2020-04-12 09:47:47 -0300

    Eliminar la cosa fea

--- Punto1/main.py
+++ Punto1/main.py
@@ -6,8 +6,4 @@ if __name__ == "__main__":
     email = E.Email()
     print(email.crearCuenta(direccion))
     print('Estimado {} te enviaremos tus mensajes a la dirección {}'.format(nombre, direccion))
-<<<<<<< HEAD
-    print('holi')
-=======
     email.cambiarContrasenia(input("Ingrese su clave para comenzar con el cambio de pass: "))
->>>>>>> e143a8a359217e599e64fdad866dcdedc5be47ff
