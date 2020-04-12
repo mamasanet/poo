2592b15fc96ad95393278129e9c159a36a73a551
AnaCarbajo <aniic.1701@gmail.com>
2020-04-12 09:56:10 -0300

    error none solucionado

--- Punto1/main.py
+++ Punto1/main.py
@@ -1,9 +1,9 @@
 import email as E
 if __name__ == "__main__":
     print('Ingrese los siguientes datos:')
-    nombre=input('nombre: ')
+    nombre= input('nombre: ')
     direccion= input('direccion email: ')
     email = E.Email()
-    print(email.crearCuenta(direccion))
+    email.crearCuenta(direccion)
     print('Estimado {} te enviaremos tus mensajes a la dirección {}'.format(nombre, direccion))
     email.cambiarContrasenia(input("Ingrese su clave para comenzar con el cambio de pass: "))
