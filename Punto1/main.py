e143a8a359217e599e64fdad866dcdedc5be47ff
andy10134 <andy10134@hotmail.com>
2020-04-12 09:18:08 -0300

    Agregada la linea de cambio de contrasenia

--- Punto1/main.py
+++ Punto1/main.py
@@ -1,8 +1,9 @@
 import email as E
 if __name__ == "__main__":
-    print('ingrese los siguientes datos:')
+    print('Ingrese los siguientes datos:')
     nombre=input('nombre: ')
     direccion= input('direccion email: ')
     email = E.Email()
     print(email.crearCuenta(direccion))
     print('Estimado {} te enviaremos tus mensajes a la dirección {}'.format(nombre, direccion))
+    email.cambiarContrasenia(input("Ingrese su clave para comenzar con el cambio de pass: "))
