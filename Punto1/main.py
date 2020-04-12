e27f863a2f3b1d5e9f623d971fa3823a7c2580da
AnaCarbajo <aniic.1701@gmail.com>
2020-04-12 13:15:20 -0300

    punto3 y test

--- Punto1/main.py
+++ Punto1/main.py
@@ -1,6 +1,15 @@
 import csv
 import email as E
 
+def test():
+    archivo     = open('C:/Users/User/Desktop/GIT/poo/Punto1/testemail.csv')
+    reader      = csv.reader(archivo, delimiter = ';')
+    for fila in reader:
+        emailtest= E.Email()
+        print(fila[0])
+        emailtest.crearCuenta(fila[0])
+
+
 def punto4(lista):
     archivo     = open('/home/andy/Escritorio/git/poo/Punto1/direccionesgmail.csv')
     reader      = csv.reader(archivo, delimiter = ';')
@@ -17,6 +26,7 @@ def punto4(lista):
     print(lista)
 
 if __name__ == "__main__":
+    test()
     print('Ingrese los siguientes datos:')
     nombre      = input('nombre: ')
     direccion   = input('direccion email: ')
@@ -25,6 +35,7 @@ if __name__ == "__main__":
     print('Estimado {} te enviaremos tus mensajes a la dirección {}'.format(nombre, direccion))
     email.cambiarContrasenia(input("Ingrese su clave para comenzar con el cambio de pass: "))
     otroemail   = E.Email()
-
+    otroemail.crearCuenta('informatica.fcefn@gmail.com')
+    print('FELICIDADES SE EJECUTO EL PUNTO3')
     listaEmails = []
     punto4(listaEmails)
